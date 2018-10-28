f03b4b7a7e3d2d2f3476e99b16d40b06e4f399c8
Mark Rivers <rivers@cars.uchicago.edu>
2018-10-28 10:05:18 -0500

    New file

---
+++ iocs/aravisIOC/iocBoot/iocAravisGigE/st.cmd
@@ -0,0 +1,63 @@
+< envPaths
+errlogInit(20000)
+
+dbLoadDatabase("$(TOP)/dbd/aravisGigEApp.dbd")
+aravisGigEApp_registerRecordDeviceDriver(pdbbase) 
+
+# Prefix for all records
+epicsEnvSet("PREFIX", "13ARV1:")
+# The port name for the detector
+epicsEnvSet("PORT",   "ARV1")
+# The queue size for all plugins
+epicsEnvSet("QSIZE",  "20")
+# The maximim image width; used for row profiles in the NDPluginStats plugin
+epicsEnvSet("XSIZE",  "1920")
+# The maximim image height; used for column profiles in the NDPluginStats plugin
+epicsEnvSet("YSIZE",  "2048")
+# The maximum number of time series points in the NDPluginStats plugin
+epicsEnvSet("NCHANS", "2048")
+# The maximum number of frames buffered in the NDPluginCircularBuff plugin
+epicsEnvSet("CBUFFS", "500")
+# The search path for database files
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+
+#aravisCameraConfig("$(PORT)", "Prosilica-02-2131A-06202")
+#dbLoadRecords("$(ARAVISGIGE)/db/Prosilica_GC.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#aravisCameraConfig("$(PORT)", "Point Grey Research-14273040")
+#dbLoadRecords("$(ARAVISGIGE)/db/PGR_Flea3.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#aravisCameraConfig("$(PORT)", "Photonic Science-V3")
+#dbLoadRecords("$(ARAVISGIGE)/db/PSL_SCMOS.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#dbLoadRecords("$(ARAVISGIGE)/db/PSL_FDI3.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#aravisCameraConfig("$(PORT)", "Point Grey Research-17165235")
+#dbLoadRecords("$(ARAVISGIGE)/db/PGR_BlackflyS_13Y3M.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#aravisCameraConfig("$(PORT)", "PointGrey-13481965")
+#dbLoadRecords("$(ARAVISGIGE)/db/PGR_Blackfly_20E4C.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#aravisCameraConfig("$(PORT)", "PointGrey-18402100")
+#dbLoadRecords("$(ARAVISGIGE)/db/PGR_Blackfly_50S5C.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#aravisCameraConfig("$(PORT)", "PointGrey-00EA4F2F")
+#dbLoadRecords("$(ARAVISGIGE)/db/PGR_GS3_U3_23S6M.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+#aravisCameraConfig("$(PORT)", "FLIR-18011754")
+#dbLoadRecords("$(ARAVISGIGE)/db/FLIR_ORX_10G_51S5M.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+aravisCameraConfig("$(PORT)", "Allied Vision Technologies-50-0503419258")
+dbLoadRecords("$(ARAVISGIGE)/db/AVT_Manta_G507C.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+asynSetTraceMask("$(PORT)",0,0x21)
+dbLoadRecords("$(ARAVISGIGE)/db/aravisCamera.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+# Create a standard arrays plugin
+NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
+# Allow for cameras up to 2048x2048x3 for RGB
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=12582912")
+
+# Load all other plugins using commonPlugins.cmd
+< $(ADCORE)/iocBoot/commonPlugins.cmd
+set_requestfile_path("$(ADPILATUS)/prosilicaApp/Db")
+
+#asynSetTraceMask("$(PORT)",0,255)
+#asynSetTraceMask("$(PORT)",0,3)
+
+
+iocInit()
+
+# save things every thirty seconds
+create_monitor_set("auto_settings.req", 30,"P=$(PREFIX)")
