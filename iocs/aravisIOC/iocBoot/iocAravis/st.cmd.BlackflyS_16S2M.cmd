3e3a0c202fe41c45fe9e6926e1acdecb5f7da768
Mark Rivers <rivers@cars.uchicago.edu>
2019-10-08 08:09:15 -0500

    New camera

--- iocs/aravisIOC/iocBoot/iocAravis/st.cmd.BlackflyS_16S2M.cmd
+++ iocs/aravisIOC/iocBoot/iocAravis/st.cmd.BlackflyS_16S2M.cmd
@@ -5,7 +5,7 @@ dbLoadDatabase("$(TOP)/dbd/ADAravisApp.dbd")
 ADAravisApp_registerRecordDeviceDriver(pdbbase) 
 
 # Name of camera as reported by arv-tool-0.6
-epicsEnvSet("CAMERA_NAME", "FLIR-011B7155")
+epicsEnvSet("CAMERA_NAME", "FLIR-Blackfly S BFS-PGE-70S7M-19069593")
 epicsEnvSet("GENICAM_DB_FILE", "$(ADGENICAM)/db/PGR_BlackflyS_16S2M.template")
 
 < st.cmd.base
